905475844ffd5433d259c9483ad7103b8bd753bb
Dmitry Deryugin <deryugin-dmitry2014@yandex.ru>
2023-07-12 10:32:36 +0300

    change in shots.scss

--- src/components/shots/shots.js
+++ src/components/shots/shots.js
@@ -1,33 +1,11 @@
-let shots = new Swiper(".shots__swiper", {
-  //   slidesPerView: 4.8,
-  grabCursor: true,
-  //   spaceBetween: 30,
-  centeredSlides: false,
-  navigation: {
-    nextEl: ".shots__swiper-next",
-    prevEl: ".shots__swiper-prev",
-  },
-  //   breakpoints: {
-  //     "@0.00": {
-  //       slidesPerView: 1,
-  //       //   spaceBetween: 30,
-  //     },
-  //     "@0.20": {
-  //       slidesPerView: 2,
-  //       //   spaceBetween: 60,
-  //     },
-  //     "@0.50": {
-  //       slidesPerView: 3,
-  //       //   spaceBetween: 50,
-  //     },
-  //     "@2.90": {
-  //       slidesPerView: 4,
-  //       //   spaceBetween: 40,
-  //     },
+// let shots = new Swiper(".shots__swiper", {
+//     slidesPerView: 4.8,
+//   grabCursor: true,
+//     spaceBetween: 30,
+//   centeredSlides: false,
+//   navigation: {
+//     nextEl: ".shots__swiper-next",
+//     prevEl: ".shots__swiper-prev",
+//   },
 
-  //     "@3.09": {
-  //       slidesPerView: 5,
-  //       spaceBetween: 30,
-  //     },
-  //   },
-});
+// });
